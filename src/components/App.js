53fd4b16023267b1fd1c78d0e687bf551f61a7ca
Mikita Kryvitski <solairerove@gmail.com>
2019-06-19 00:54:28 +0300

    feature: make api request and display images

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,34 @@
 import React from 'react';
+import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
+import ImageList from './ImageList';
 
 class App extends React.Component {
 
-    onSearchSubmit(term) {
-        console.log(term);
+    state = { images: [] }
+
+    // async/await or just .then(next => {}) as Promise
+    // and
+    // <SearchBar onSubmit={(e) => this.onSearchSubmit(e)} />
+    // or
+    // onSearchSubmit = async (term) => { }
+    // or
+    // constructor this binding
+    onSearchSubmit = async (term) => {
+        const response = await unsplash.get('https://api.unsplash.com/search/photos', {
+            params: {
+                query: term
+            }
+        });
+
+        this.setState({ images: response.data.results });
     }
 
     render() {
         return (
             <div className="ui container" style={{ marginTop: '10px' }}>
                 <SearchBar onSubmit={this.onSearchSubmit} />
+                <ImageList images={this.state.images} />
             </div>
         );
     }
---
+++ src/components/ImageList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// destruction
+const ImageList = (props) => {
+    const images = props.images.map(({ id, urls, alt_description }) => {
+        return <img key={id} src={urls.regular} alt={alt_description} />
+    });
+
+    return <div>{images}</div>
+};
+
+export default ImageList;
